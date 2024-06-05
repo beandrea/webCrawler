ee1d6145f2ae5d41e5dd9c75f8bbc15a0cf54880
beandrea <arkflame99@gmail.com>
2024-06-04 20:11:50 -0400

    normalize

---
+++ crawl.js
@@ -0,0 +1,12 @@
+const URL = require('node:url');
+
+const normalizeURL = (url) => {
+    const urlObj = new URL(url);
+    let fullPath = `${urlObj.host}${urlObj.pathname}`
+    if (fullPath.slice(-1) === '/') {
+        fullPath = fullPath.slice(0, -1)
+    }
+    return fullPath
+}
+
+export { normalizeURL }
